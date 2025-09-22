f8661db9da700d32f2ef6e911d6b9a515aac16a6
KHSBE <105274579+KHSBE@users.noreply.github.com>
2025-09-22 12:14:43 +0900

    feat: 수집 시스템 통합 서비스 추가 및 자잘한 주석 수정

--- src/main/java/com/compass/domain/chat/collection/service/TravelInfoCollector.java
+++ src/main/java/com/compass/domain/chat/collection/service/TravelInfoCollector.java
@@ -2,7 +2,7 @@ package com.compass.domain.chat.collection.service;
 
 import com.compass.domain.chat.model.request.TravelFormSubmitRequest;
 
-// Task 2.2.1: 정보 수집 인터페이스
+//  정보 수집 인터페이스
 public interface TravelInfoCollector {
 
     // 사용자 입력으로부터 여행 정보를 수집(업데이트)
